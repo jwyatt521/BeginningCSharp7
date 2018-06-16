28ccabf375c7a5b99de34c493828b0a02df387cb
Joseph Wyatt <jwyatt521@gmail.com>
2018-06-16 04:22:08 -0400

    Uploaded Chapter03 of my learnings

---
+++ Chapter03/CH03Ex02/CH03Ex02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Convert;
+
+namespace CH03Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                string userName;
+                WriteLine("Welcome to the consoleBar.NET, type your name and press \"enter\" ");
+                userName = ReadLine();
+                WriteLine($"What'd you do, just mash letters on the keyboard? Ok, I'll allow it (but I'm " +
+                    $"watching you). So, {userName}, you're here for some alcohol.exe? Type your age and then " +
+                    $"press \"enter\" ");
+                int age = ToInt32(ReadLine());
+                if (age >= 21)
+                    WriteLine($"Let's get {userName}'s download started, eh?");
+                else
+                    WriteLine($"Hey {userName}, you like appleJuice.exe? " +
+                        $"Get outa here with your underage self, ya bot.");
+
+                WriteLine($"Would you like to interact with bartemder.exe again? Type:Yes or No? ");
+            } while (ReadLine() == Yes);
+        }
+    }
+}
---
+++ Chapter03/Ch03Ex01/Ch03Ex01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace Ch03Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInteger;
+            string myString;
+            myInteger = 17;
+            myString = "myInteger is";
+            WriteLine($"{myString}\n {myInteger}");
+            ReadKey();
+        }
+    }
+}
