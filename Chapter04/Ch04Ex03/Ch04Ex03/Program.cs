f6ee371d36a577530357ed70d10a3d87c4ac4db4
Joseph Wyatt <jwyatt521@gmail.com>
2018-06-16 04:24:36 -0400

    Uploaded Chapter04

---
+++ Chapter04/Ch04Ex03/Ch04Ex03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Convert;
+
+
+namespace Ch04Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "benjamin";
+            const
+        }
+    }
+}
