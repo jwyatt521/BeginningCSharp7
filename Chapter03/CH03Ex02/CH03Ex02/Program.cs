4f528e809fdbdb2d1bda822ad0d20ec4c136c78b
Joseph Wyatt <jwyatt521@gmail.com>
2018-06-16 05:09:39 -0400

    Updated the app process
    The previous app was one that wasn't actually my Ch03Ex02, but was the beginning code for Assignment 02. I updated the file to reflect what was actually supposed to be here.

--- Chapter03/CH03Ex02/CH03Ex02/Program.cs
+++ Chapter03/CH03Ex02/CH03Ex02/Program.cs
@@ -12,23 +12,26 @@ namespace CH03Ex02
     {
         static void Main(string[] args)
         {
-            do
-            {
-                string userName;
-                WriteLine("Welcome to the consoleBar.NET, type your name and press \"enter\" ");
-                userName = ReadLine();
-                WriteLine($"What'd you do, just mash letters on the keyboard? Ok, I'll allow it (but I'm " +
-                    $"watching you). So, {userName}, you're here for some alcohol.exe? Type your age and then " +
-                    $"press \"enter\" ");
-                int age = ToInt32(ReadLine());
-                if (age >= 21)
-                    WriteLine($"Let's get {userName}'s download started, eh?");
-                else
-                    WriteLine($"Hey {userName}, you like appleJuice.exe? " +
-                        $"Get outa here with your underage self, ya bot.");
-
-                WriteLine($"Would you like to interact with bartemder.exe again? Type:Yes or No? ");
-            } while (ReadLine() == Yes);
+            double firstNumber, secondNumber;
+            string userName;
+            WriteLine("Enter your name:");
+            userName = ReadLine();
+            WriteLine($"Welcome {userName}");
+            WriteLine($"Now give me a number:");
+            firstNumber = ToDouble(ReadLine());
+            WriteLine("Now give me a second number:");
+            secondNumber = ToDouble(ReadLine());
+            WriteLine($"The sum of {firstNumber} and {secondNumber} is " +
+                $"{firstNumber + secondNumber}.");
+            WriteLine($"The result of subtracting {firstNumber} from " +
+                $"{secondNumber} is {firstNumber - secondNumber}.");
+            WriteLine($"The product of {firstNumber} and {secondNumber} is " +
+                $"{firstNumber * secondNumber}.");
+            WriteLine($"The result of dividing {firstNumber} by {secondNumber}" +
+                $" is {firstNumber / secondNumber}.");
+            WriteLine($"The remainder after dividing {firstNumber} by " +
+                $"{secondNumber} is {firstNumber % secondNumber}.");
+            ReadKey();
         }
     }
 }
