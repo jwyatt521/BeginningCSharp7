9483c2428f1e16cbe980f6fffeea92fb70ee4410
jwyatt521 <jwyatt521@gmail.com>
2018-06-16 14:17:50 -0400

    Switch Statements

--- Chapter04/Ch04Ex03/Ch04Ex03/Program.cs
+++ Chapter04/Ch04Ex03/Ch04Ex03/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
-using static System.Convert;
 
 
 namespace Ch04Ex03
@@ -14,7 +13,25 @@ namespace Ch04Ex03
         static void Main(string[] args)
         {
             const string myName = "benjamin";
-            const
+            const string niceName = "andrea";
+            const string sillyName = "ploppy";
+            string name;
+            WriteLine("What is your name?");
+            name = ReadLine();
+            switch (name.ToLower())
+            {
+                case myName:
+                    WriteLine("You have the same name as me!");
+                    break;
+                case niceName:
+                    WriteLine("My, what a nice name you have!");
+                    break;
+                case sillyName:
+                    WriteLine("That's a very silly name.");
+                    break;
+            }
+            WriteLine($"Hello {name}!");
+            ReadKey();
         }
     }
 }
