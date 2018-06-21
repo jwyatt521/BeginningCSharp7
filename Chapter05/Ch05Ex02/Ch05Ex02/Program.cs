1b42232480672a57ac0f859588d5dc9295e29a5d
jwyatt521 <jwyatt521@gmail.com>
2018-06-20 21:23:07 -0400

    Using an Enumeration

--- Chapter05/Ch05Ex02/Ch05Ex02/Program.cs
+++ Chapter05/Ch05Ex02/Ch05Ex02/Program.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
+using static System.Convert;
 
 namespace Ch05Ex02
 {
+    enum orientation : byte
+    {
+        north = 1,
+        south = 2,
+        east  = 3,
+        west  = 4
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            byte directionByte;
+            string directionString;
+            orientation myDirection = orientation.north;
+            WriteLine($"myDirection = {myDirection}");
+            directionByte = (byte)myDirection;
+            directionString = Convert.ToString(myDirection);
+            WriteLine($"byte equivalent = {directionByte}");
+            WriteLine($"string equivalent = {directionString}");
+            ReadKey();
         }
     }
 }
