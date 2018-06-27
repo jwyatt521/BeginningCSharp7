edc850413944ee2175f315124e45a541ddde9c5f
jwyatt521 <jwyatt521@gmail.com>
2018-06-27 16:32:03 -0400

    Using an Array

---
+++ Chapter05/Ch05Ex04/Ch05Ex04/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch05Ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friendNames = { "Josiah James", "David Scott", "Hannah Jean" };
+            int i;
+            WriteLine($"Here are {friendNames.Length} of my frinds:");
+            for (i = 0; i < friendNames.Length; i++)
+            {
+                WriteLine(friendNames[i]);
+            }
+            ReadKey();
+        }
+    }
+}
