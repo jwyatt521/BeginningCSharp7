5678f7899ad2a9c460c7160168f75e1ce88ac902
jwyatt521 <jwyatt521@gmail.com>
2018-06-27 17:20:35 -0400

    Statement Auto-Completetion in Visual Studio

---
+++ Chapter05/Ch05Ex06/Ch05Ex06/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch05Ex06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "This is a test.";
+            char[] seperator = { ' ' };
+            string[] myWords;
+            myWords = myString.Split(seperator);
+            foreach (string word in myWords)
+            {
+                WriteLine($"{word}");
+            }
+            ReadKey();
+        }
+    }
+}
